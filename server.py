17ed2ad7e9c1d55b849f4828e03d52d0dabb585d
gilons <gf.694765457@gmail.com>
2025-06-25 09:48:45 +0100

    Always allow all origins

--- server.py
+++ server.py
@@ -78,8 +78,8 @@ def create_cors_middleware(allowed_origin: str):
             response = await handler(request)
 
         response.headers['Access-Control-Allow-Origin'] = allowed_origin
-        response.headers['Access-Control-Allow-Methods'] = 'POST, GET, DELETE, PUT, OPTIONS'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        response.headers['Access-Control-Allow-Methods'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = '*'
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         return response
 
